254c8ef35fef636948d85d074ec67ebb1af9b241
mskorczak <mskorczak1@gmail.com>
2023-10-23 21:34:42 +0100

    Added build script

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+BUILDFILES=("resume.tex","altacv.cls","README.md","build.sh","output.txt","resume.pdf")
+echo "Building resume and deleting output.txt"
+rm output.txt
+echo "Building resume" | tee -a output.txt
+echo -e "Generating resume PDF" | tee -a output.txt
+pdflatex resume.tex -o resume.pdf > /dev/null
+echo -e "Deleting build files" | tee -a output.txt
+FILES=$(ls -r)
+for file in $FILES
+do
+	if [[ " ${BUILDFILES[*]} " != *"$file"* ]];
+	then
+		rm $file
+	fi
+done
+echo -e "Done." | tee -a output.txt
