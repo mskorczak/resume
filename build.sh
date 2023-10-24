103109f0e8d75d4cb817537c00801bb18495fe53
mskorczak <mskorczak1@gmail.com>
2023-10-24 18:15:47 +0100

    Added PNG build step, changed to 'mskor_...'

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-BUILDFILES=("resume.tex","altacv.cls","README.md","build.sh","output.txt","resume.pdf")
+BUILDFILES=("mskor_resume.tex","altacv.cls","README.md","build.sh","output.txt","mskor_resume.pdf","mskor_resume.png")
 echo "Building resume and deleting output.txt"
 rm output.txt
 echo "Building resume" | tee -a output.txt
 echo -e "Generating resume PDF" | tee -a output.txt
-pdflatex resume.tex -o resume.pdf > /dev/null
+pdflatex mskor_resume.tex -o mskor_resume.pdf > /dev/null
 echo -e "Deleting build files" | tee -a output.txt
 FILES=$(ls -r)
 for file in $FILES
@@ -14,4 +14,6 @@ do
 		rm $file
 	fi
 done
+echo -e "Creating PNG version" | tee -a output.txt
+convert -density 150 mskor_resume.pdf -quality 90 mskor_resume.png
 echo -e "Done." | tee -a output.txt
