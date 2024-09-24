584d28a94a3486abcf83230b06002620630442c9
mskorczak <mskorczak1@gmail.com>
2024-09-24 21:08:21 +0100

    Updated: simplified the entire thing

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-BUILDFILES=("mskor_resume.tex","altacv.cls","README.md","build.sh","output.txt","mskor_resume.pdf")
+BUILDFILES=("resume.tex","altacv.cls","README.md","build.sh","output.txt","resume.pdf")
 echo "Building resume and deleting output.txt"
 rm output.txt
 echo "Building resume" | tee -a output.txt
 echo -e "Generating resume PDF" | tee -a output.txt
-pdflatex mskor_resume.tex -o mskor_resume.pdf > /dev/null
+pdflatex resume.tex -o resume.pdf > /dev/null
 echo -e "Deleting build files" | tee -a output.txt
 FILES=$(ls -r)
 for file in $FILES
@@ -15,5 +15,5 @@ do
 	fi
 done
 echo -e "Creating PNG version" | tee -a output.txt
-convert -density 300 -colorspace sRGB mskor_resume.pdf -alpha off mskor_resume.png
+convert -density 300 -colorspace sRGB resume.pdf -alpha off resume.png
 echo -e "Done." | tee -a output.txt
